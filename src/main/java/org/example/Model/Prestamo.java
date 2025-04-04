2d4fa8e0d64fbda30ab3fd2fbaf8006059ff73e2
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-03 22:02:30 -0600

    Creamos la clase Prestamo con sus constructores

--- src/main/java/org/example/Model/Prestamo.java
+++ src/main/java/org/example/Model/Prestamo.java
@@ -1,5 +1,50 @@
 package org.example.Model;
 
+import java.time.LocalDate;
+
 public class Prestamo {
 
+    private Libro libro;
+    private Usuario usuario;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(Libro libro, Usuario usuario, LocalDate fechaPrestamo, LocalDate fechaDevolucion) {
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = LocalDate.now();
+        this.fechaDevolucion = null;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
 }
