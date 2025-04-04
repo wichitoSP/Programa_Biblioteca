fec8d0c2fbdac96c8b5b00cd1c8abfe05ad96f7f
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-04 09:58:09 -0600

    Creamos la vista Libro a nuestro proyecto

---
+++ src/main/java/org/example/vistas/Libro.java
@@ -0,0 +1,11 @@
+package org.example.vistas;
+
+import javax.swing.*;
+
+public class Libro {
+    private JButton registarButton;
+    private JTextField textField1;
+    private JTextField Autor;
+    private JTextField isbn;
+    private JPanel Titulo;
+}
