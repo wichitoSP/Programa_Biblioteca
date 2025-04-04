2cd6212ec9be5150a353d9a4f16aaf866f53c301
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-04 10:26:45 -0600

    Creamos Prestamo formulario

---
+++ src/main/java/org/example/vistas/Prestamo.java
@@ -0,0 +1,11 @@
+package org.example.vistas;
+
+import javax.swing.*;
+
+public class Prestamo {
+    private JTextField Libro;
+    private JTextField usuarioTextField;
+    private JTextField fechaPrestamoTextField;
+    private JTextField FechaDevolucion;
+    private JButton Registrar;
+}
