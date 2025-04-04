606e8f6d1a2a111910505343fa35501732366790
wichitoSP <99522188+wichitoSP@users.noreply.github.com>
2025-04-03 21:56:34 -0600

    Creamos la clase usuario con sus constructores

--- src/main/java/org/example/Model/Usuario.java
+++ src/main/java/org/example/Model/Usuario.java
@@ -1,4 +1,28 @@
 package org.example.Model;
 
 public class Usuario {
+
+    private String id;
+    private String nombre;
+
+    public Usuario(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
